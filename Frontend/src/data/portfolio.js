2ec64f0b253c263eeb27397e7df2788dfa7ce145
Kapil Keer <kapilkeer1998@gmail.com>
2026-09-21 13:00:51 +0530

    update project  content

--- Frontend/src/data/portfolio.js
+++ Frontend/src/data/portfolio.js
@@ -37,7 +37,7 @@ export const portfolio = {
       // impact:
       //   'Built with a focus on structured backend logic, maintainable feature modules, and a user experience that can scale as engagement grows.',
       features: [
-        'Implemented secure **Google OAuth 2.0 authentication** with custom JWT Access/Refresh Token generation and hashed refresh-token storage.',
+        'Implemented secure Google OAuth 2.0 authentication with custom JWT Access/Refresh Token generation and hashed refresh-token storage.',
         'Built automatic access-token renewal and logout-from-all-devices functionality for secure session management and revocation.',
         'Added OTP verification, real-time login alerts, secure logout',
         'Built real-time one-to-one messaging using Socket.IO for instant user communication.',
